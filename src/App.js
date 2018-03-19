82bf6ff8ef4e0b2935b0f93a28ab7e30f2bab4c2
Freddy8 <Contactfreddy11@Gmail.com>
2018-03-18 21:59:35 -0700

    assigment-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
+
 
 class App extends Component {
   render() {
@@ -13,6 +17,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/Album" component={Album} />
+        </main>
       </div>
     );
   }
