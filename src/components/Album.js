7495bcc1563f5911f0d53d68f227a751116c03f2
Freddy8 <Contactfreddy11@Gmail.com>
2018-04-12 22:21:02 -0700

    bs

--- src/components/Album.js
+++ src/components/Album.js
@@ -18,20 +18,34 @@ class Album extends Component {
     return (
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} />
+          <img alt="album-art" id="album-cover-art" src={this.state.album.albumCover} />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
-        <table id ="song-list">
+        <table id="song-list">
           <colgroup>
-            <col id ="song-number-column" />
+            <col id="song-number-column" />
             <col id="song-title-column" />
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+            <tr>
+              <th>Number</th>
+              <th>Title</th>
+              <th>Duration</th>
+            </tr>
+          {
+            this.state.album.songs.map( (song, index) => (
+              <tr className="songs" key={index}>
+                <td>{index + 1}</td>
+                <td>{song.title}</td>
+                <td>{song.duration}</td>
+              </tr>
+            ))
+          }
           </tbody>
         </table>
       </section>
